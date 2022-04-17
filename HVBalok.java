31fa89d98664e89f4fcd75c8e3f49ed25808bf95
KaNgantok <102915227+KaNgantok@users.noreply.github.com>
2022-04-18 00:31:49 +0800

    send tugas

---
+++ HVBalok.java
@@ -0,0 +1,20 @@
+package hvbalok;
+
+/**
+ *
+ * @author Dani Pratama_20103320
+ */
+public class HVBalok {
+
+    
+    public static void main(String[] args) {
+        
+        VBalok balok = new VBalok(0,0 ,0 );
+        balok.setLebar(15);
+        balok.setPanjang(20);
+        balok.setTinggi(8);
+        balok.tapvol();
+        
+    }
+    
+}
