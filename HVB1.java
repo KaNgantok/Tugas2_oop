68950c1f2549b19161568bc6e36aa8b7ab6d0ab1
KaNgantok <102915227+KaNgantok@users.noreply.github.com>
2022-04-18 01:03:37 +0800

    public

---
+++ HVB1.java
@@ -0,0 +1,56 @@
+package vb1;
+
+/**
+ *
+ * @author Dani Pratama_20103320
+ */
+public class HVB1 {
+    
+    public double pj, lb, tg, vol;
+    
+    public HVB1 (double Pj, double Lb, double Tg){
+        
+        this.setPanjang(Pj);
+        this.setLebar(Lb);
+        this.setTinggi(Tg);
+        
+    }
+    
+   public void setPanjang(double Pj){
+        
+        this.pj=Pj;
+        
+    }
+    
+    public void setLebar(double Lb){
+        
+        this.lb=Lb;
+        
+    }
+    
+    public void setTinggi (double Tg){
+        
+        this.tg=Tg;
+        
+    }
+    
+    public double HV(){
+        
+        this.vol= this.pj * this.lb * this.tg; 
+        return this.vol;
+        
+    }
+    
+    public void tapvol(){
+        
+        this.HV();
+        System.out.println("Hitung Volume Balok");
+        System.out.println("======================");
+        System.out.println("Nilai Panjang = "+this.pj+"cm²");
+        System.out.println("Nilai Lebar = "+this.lb+"cm²");
+        System.out.println("Nilai Tinggi = "+this.tg+"cm²");
+        System.out.println("Nilai Volume Balok = "+this.vol+"cm³");
+        
+    }
+    
+}
---
+++ VB1.java
@@ -0,0 +1,19 @@
+package vb1;
+
+/**
+ *
+ * @author Dani Pratama_20103320
+ */
+public class VB1 {
+
+public static void main(String[] args) {
+        
+        HVB1 balok = new HVB1(0,0 ,0 );
+        balok.setLebar(7);
+        balok.setPanjang(10);
+        balok.setTinggi(15);
+        balok.tapvol();
+    
+    }
+    
+}
